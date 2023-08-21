153d4ec9f36aee5ba984695c8f9158186bd7d641
Zeeshan <1966tumhassan@gmail.com>
2023-08-22 04:13:22 +0530

    basic backend setup

--- Backend/src/routes/trips.js
+++ Backend/src/routes/trips.js
@@ -1,1 +1,21 @@
 
+const express = require('express');
+const router = express.Router();
+
+// Create a new trip
+router.post('/create', (req, res) => {
+  // Handle trip creation logic here
+});
+
+// Get all trips
+router.get('/all', (req, res) => {
+  // Handle fetching all trips
+});
+
+// Join a trip
+router.post('/join/:tripId', (req, res) => {
+  const tripId = req.params.tripId;
+  // Handle joining a trip
+});
+
+module.exports = router;
