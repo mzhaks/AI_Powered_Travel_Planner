41953d5dc671ba14c7668cbb98b18d85bf39a026
MJakash <akupadhiya7388@gmail.com>
2023-09-06 17:26:10 +0530

    Adding a file

---
+++ client/src/AppJs (2).js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+//  import Navbar from './component/Navbar';
+//import Login from './component/Login';
+//import Signup from './component/Signup';
+import Landingpage from './component/Landingpage';
+
+function App() {
+  return (
+    <div className="App">
+{/* <Navbar/> */}
+{/* <Login/> */}
+{/* <Signup/> */}
+<Landingpage />
+    </div>
+  );
+}
+
+export default App;
